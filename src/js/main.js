2feee0a07d7d70a195719aee63f5e9fb50c673c5
Thomas Francis <86661302+Tom2001moT@users.noreply.github.com>
2023-12-17 23:00:01 +0530

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+    var loadingScreen = document.getElementById('loading-screen');
+    setTimeout(function() {
+      loadingScreen.style.display = 'none';
+    }, 3000);
+  });
+
+  
